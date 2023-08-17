a2ce993abaff1ded062e479510a02cbeb660bae6
Sarah Goewey <sarah.goewey@yahoo.com>
2023-08-17 16:07:25 -0400

    more initial setup

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.static("static"));
 
+app.get("/", (req, res) => {
+  res.send(
+    `
+    <html>
+      <h1>Sarah Goewey</h1>
+      <h2>backup-cash-register.onrender.com</h2>
+    </html>
+    `
+  );
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
